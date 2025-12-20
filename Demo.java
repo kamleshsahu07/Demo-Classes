7a885ccf7fdf48ce54e49d6561fd9c35e2b7b4ad
kamleshsahu07 <kamleshsahu66268@gmail.com>
2025-12-20 06:59:42 +0530

    arrays

--- Demo.java
+++ Demo.java
@@ -1,105 +1,57 @@
-// public class Demo{
-//     public static void main(String[] args) {
-//     //     for(int i=1;i<=5;i++) {
-// 	// 		for(int j=1;j<=5;j++) {
-// 	// 			if(i==1 || j==1 || j==5 || i==5) {
-// 	// 				System.out.print("*");
-// 	// 			}
-// 	// 			else {
-// 	// 				System.out.print(" ");
-// 	// 			}
-// 	// 	}
-// 	// 		System.out.println();
-// 	// }
-
-
-// 		for(int i=1;i<=5;i++){
-// 			for(int j=1;j<=5;j++){
-// 				if(i>=j){
-// 					System.out.print("*");
-// 				}
-// 			}
-// 			System.out.println();
-// 		}
-
-// 	}}
-    
-
+import java.util.*;
 public class Demo{
-	public static void main(String[] args) {
-		Robot R1=new Robot();
-		R1.Has("Chitti",2,21);
-		System.out.println(R1.Name);
-		System.out.println(R1.age);
-		System.out.println(R1.Roll_NO);
-		R1.Does("Running, Walking, Gaming, Cleaning");
-		R1.Emotions("No Emotions");
+	public static void main(String[] args){
+
+//       ARRAYS 
+ 		Scanner sc=new Scanner(System.in);
+		int array[]=new int[5];
+		for(int i=0;i<5;i++){
+			array[i]=sc.nextInt();
+		}
+		for(int i=0;i<array.length;i++){
+			System.out.println("marks = "+array[i]);
+		}
 
 
-		Car c1=new Car();
-		c1.Setter("Tesla","Maroon",4,"100Kwh");
-		c1.Has();
-		c1.Does("Fast with Rapid innovation");
-		c1.SpecialF("Self Drive, Bio weapon Defense Mode");
+
+//          -- -- Taking No of Employee And Store Their name in Arrays
+
+		System.out.println("please enter the name of employee");
+		int n=sc.nextInt();
+		sc.nextLine();
+		String a[]=new String[n];
+	
+		for(int k=0;k<a.length;k++) {
+		System.out.println("Please enter the name of employee "+(k+1));
+		a[k]=sc.nextLine();
+		}
+	
+	//o/p
+		for(int l=0;l<a.length;l++) {
+		System.out.print(a[l]+" ");
+		}	
+	
 		System.out.println();
-		System.out.println("----- NEXT ----");
-		System.out.println();
-		Car c2=new Car();
-		c2.Setter("BMW","Blue",4,"2998 cc");
-		c2.Has();
-		c2.Does("Fast running with Safety, Voice Assitance");
-		c2.SpecialF("3D remote View, Curb view");
 
+		
+//       ------ 2D Arrays --------------       ///
 
-
-	}
-}
-class Robot{
-	String Name;
-	int age;
-	int Roll_NO;
-	String work;
-	void Has(String Name,int age,int Roll_NO){
-		this.Name=Name;
-		this.age=age;
-		this.Roll_NO=Roll_NO;
-	}
-	void Does(String work){
-		System.out.println(work);
-	}
-	void Emotions(String Emotions){
-		// this.work=work;
-		System.out.println(Emotions);
-	}
-}
-
-class Car{
-	String Name;
-	String Color;
-	int Wheels;
-	String CC_Engine;
-	String SpecialF;
-	String Does;
-
-	void Setter(String Name,String Color,int Wheels,String CC_Engine){
-		this.Name=Name;
-		this.Color=Color;
-		this.Wheels=Wheels;
-		this.CC_Engine=CC_Engine;
-		this.Does=Does;
-		this.SpecialF=SpecialF;
-	}
-    void Has(){
-		System.out.println(Name);
-		System.out.println(Color);
-		System.out.println(Wheels);
-		System.out.println(CC_Engine);
-	}
-	void Does(String Does){
-		System.out.println(Does);
-	}
-	void SpecialF(String SpecialF){
-		System.out.println(SpecialF);
-	}
-
+		int arrays[][]=new int[3][5];
+		for(int b=0;b<3;b++){
+			System.out.println("Enter the marks of class = "+b);
+			for(int j=0;j<arrays[b].length;j++){
+				System.out.println("Enter the marks of class "+b+" -> "+j+"th Student");
+				arrays[b][j]=sc.nextInt();
+			}
+		}
+		int b=arrays[0].length;
+		System.out.println(a);
+		for(int m=0;m<3;m++){
+			System.out.println("Class "+m+" Students ");
+			for(int j=0;j<5;j++){
+				System.out.print(arrays[m][j]+" ");
+			}
+			System.out.println();
+		}
+		}
 }
